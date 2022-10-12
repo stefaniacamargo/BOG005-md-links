b8141b23974fd4950616753d3ffc4230038a363e
stefaniacamargo <estefaniacamargo263@gmail.com>
2022-10-12 16:32:05 -0500

    functions read files and directories

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+const mdLinks = require('./main');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const chalk = require("chalk");
 const { argv } = yargs(hideBin(process.argv));
 
 if (argv.validate) {
@@ -15,3 +17,10 @@ if (argv.stats) {
 } else {
   console.log('No show stats')
 }
+
+Promise.all(mdLinks(argv._[0])).then((fileLinks) => {
+  fileLinks.forEach(links => {
+    console.log(links);
+    console.log(chalk.red('-------------------------------'));
+  })
+})
--- src/main.js
+++ src/main.js
@@ -1,16 +1,70 @@
-const chalk = require('chalk');
-const path = require('path');
-const fs = require('fs');
+const chalk = require("chalk");
+const path = require("path");
+const fs = require("fs");
+const { marked } = require("marked");
 
-const dir = fs.readdirSync(path.resolve(__dirname));
+console.log(chalk.blue("Hello world!"));
 
-dir.forEach(fileName => {
-  fs.readFile(path.resolve(__dirname, fileName), 'utf8', (error, data) => {
-    if (error) throw error;
-    console.log(data);
-    console.log('------------------------');
+/**
+ * 
+ * @param {string} fullPath 
+ * @param {Object} [options]
+ * @param {boolean} [options.validate]
+ * @param {boolean} [options.stats]
+ * @returns 
+ */
+function readMdFile(fullPath, options) {
+  return new Promise((resolve, reject) => {
+    const links = [];
+    const isMarkdown = path.extname(fullPath) === ".md";
+    if (!isMarkdown) {
+      resolve(links);
+    }
+    fs.readFile(fullPath, "utf8", (error, data) => {
+      if (error) reject(error);
+      marked(data, {
+        walkTokens: (token) => {
+          if (token.type === "link" && token.href.includes("http")) {
+            links.push({
+              href: token.href,
+              text: token.text,
+              file: fullPath,
+            });
+          }
+        },
+      });
+      resolve(links);
+    });
   });
-});
+}
 
-console.log(chalk.blue('Hello world!'));
+/**
+ * 
+ * @param {string} filePath 
+ * @param {Object} [options]
+ * @param {boolean} [options.validate]
+ * @param {boolean} [options.stats]
+ * @returns 
+ */
+function mdLinks(filePath, options) {
+  const resolvedPath = path.resolve(filePath);
+  const pathExist = fs.existsSync(resolvedPath);
+  const pendingFilesToRead = [];
+  if (!pathExist) {
+    throw new Error("The provided path doesn't exist");
+  }
+  const isDirectory = fs.lstatSync(resolvedPath).isDirectory();
+  if (isDirectory) {
+    const dir = fs.readdirSync(resolvedPath, { withFileTypes: true });
+    dir.forEach((fileName) => {
+      if (fileName.isFile()) {
+        pendingFilesToRead.push(readMdFile(path.resolve(resolvedPath, fileName.name)));
+      }
+    });
+  } else {
+    pendingFilesToRead.push(readMdFile(path.resolve(resolvedPath)));
+  }
+  return pendingFilesToRead;
+}
 
+module.exports = mdLinks;
